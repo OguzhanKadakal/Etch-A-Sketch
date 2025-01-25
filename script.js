0eabf7a88603cb7cee2fcb9cd51e8b7e927df98a
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-01-25 16:41:35 +0300

    Added asset files and generated 16x16 grid dynamically using JavaScript

---
+++ script.js
@@ -0,0 +1,21 @@
+//Select the root element.
+const rootElement = document.querySelector("#root");
+
+//Create a new div and added container class and appended to the root
+const containerDiv = document.createElement("div");
+containerDiv.classList.add("container");
+rootElement.appendChild(containerDiv);
+
+//Select the new container div
+const container = document.querySelector(".container");
+
+//Create 16*16 grid = 256 squares
+const gridSize = 16;
+const totalSquares = gridSize * gridSize;
+
+for (let i = 0; i < totalSquares; i++) {
+    const square = document.createElement("div"); //Creates new div
+    square.classList.add("grid-square"); //Add class to the new square div
+    container.appendChild(square); //Add new element to the container div
+}
+
